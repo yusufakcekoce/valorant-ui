2887cff7c9b78bb8cde950afe67e8d76c027e0be
Yusuf Akçekoce <yakcekoce@gmail.com>
2022-04-27 18:39:08 +0300

    Details Page Edited

--- src/context/AgentPages.js
+++ src/context/AgentPages.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "../styles/AgentPagesStyle.css";
 
 const CardDetails = () => {
   let { uuid } = useParams();
   let [fetchedData, updateFetchedData] = useState([]);
-  let { data, displayName, displayIcon, description } = fetchedData
+  let { data, abilities, role, displayName, displayIcon, description } =
+    fetchedData;
   console.log(fetchedData);
 
   let api = `https://valorant-api.com/v1/agents/${uuid}`;
@@ -15,7 +17,88 @@ const CardDetails = () => {
     })();
   }, [api]);
 
-  return <div>The details of the card are here - {uuid}</div>;
+  return (
+    <div className="ap-container">
+      <div className="ap-left-bar">
+        <div className="ap-img">
+          <img src={data?.background} alt="" width="400px" className="img1" />
+          <img
+            src={data?.fullPortraitV2}
+            alt=""
+            width="400px"
+            className="img2"
+          />
+        </div>
+        <div className="ap-details">
+          <p>{data?.displayName}</p>
+          <p>{data?.role?.displayName}</p>
+        </div>
+      </div>
+      <div className="ap-content">
+        <div className="ap-content-box">
+          <ul className="ap-list">
+            <li>
+              <p className="skill-head">
+                {data && data.abilities[0].displayName}
+              </p>
+              <p>
+                {data && data.abilities[0].description}
+                <img
+                  src={data && data.abilities[0].displayIcon}
+                  alt=""
+                  width="50px"
+                />
+              </p>
+              <hr />
+            </li>
+            <li>
+              <p className="skill-head">
+                {data && data.abilities[1].displayName}
+              </p>
+              <p>
+                {data && data.abilities[1].description}
+                <img
+                  src={data && data.abilities[1].displayIcon}
+                  alt=""
+                  width="50px"
+                />
+              </p>
+              <hr />
+            </li>
+            <li>
+              <p className="skill-head">
+                {data && data.abilities[2].displayName}
+              </p>
+              <p>
+                {data && data.abilities[2].description}
+                <img
+                  src={data && data.abilities[2].displayIcon}
+                  alt=""
+                  width="50px"
+                />
+              </p>
+              <hr />
+            </li>
+            <li>
+              <p className="skill-head">
+                {data && data.abilities[3].displayName}
+              </p>
+              <p>
+                {data && data.abilities[3].description}
+                <img
+                  src={data && data.abilities[3].displayIcon}
+                  alt=""
+                  width="50px"
+                />
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardDetails;
+
+// data.abilities[0].description
